2e65b00dedce38ae29eb3d52f9563f19ccac19df
glshop <117030712+glshop@users.noreply.github.com>
2023-10-13 17:54:16 +0300

    comit

--- project.py
+++ project.py
@@ -19,12 +19,12 @@ conn.commit()
 
 def add_contact():
     name = name_entry.get()
-    email = email_entry.get()
-    cursor.execute('INSERT INTO contacts (name, email) VALUES (?, ?)', (name, email))
+    phone = phone_entry.get()
+    cursor.execute('INSERT INTO contacts (name, email) VALUES (?, ?)', (name, phone))
     conn.commit()
     status_label.config(text="Контакт додано!")
     name_entry.delete(0, tk.END)
-    email_entry.delete(0, tk.END)
+    phone_entry.delete(0, tk.END)
 
 # Функція для виведення всіх контактів
 def list_contacts():
@@ -50,10 +50,10 @@ name_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
 name_entry = ttk.Entry(frame)
 name_entry.grid(row=0, column=1, padx=5, pady=5, sticky="w")
 
-email_label = ttk.Label(frame, text="Email:")
-email_label.grid(row=1, column=0, padx=5, pady=5, sticky="w")
-email_entry = ttk.Entry(frame)
-email_entry.grid(row=1, column=1, padx=5, pady=5, sticky="w")
+phone_label = ttk.Label(frame, text="Номер Телефону:")
+phone_label.grid(row=1, column=0, padx=5, pady=5, sticky="w")
+phone_entry = ttk.Entry(frame)
+phone_entry.grid(row=1, column=1, padx=5, pady=5, sticky="w")
 
 add_button = ttk.Button(frame, text="Додати контакт", command=add_contact)
 add_button.grid(row=2, column=0, columnspan=2, pady=10)
@@ -64,14 +64,14 @@ list_button.grid(row=3, column=0, columnspan=2, pady=10)
 status_label = ttk.Label(frame, text="", foreground="green")
 status_label.grid(row=4, column=0, columnspan=2)
 
-contact_list = ttk.Treeview(frame, columns=("ID", "Ім'я", "Email"), show="headings")
+contact_list = ttk.Treeview(frame, columns=("ID", "Ім'я", "Номер Телефону"), show="headings")
 contact_list.heading("ID", text="ID")
 contact_list.heading("Ім'я", text="Ім'я")
-contact_list.heading("Email", text="Email")
+contact_list.heading("Номер Телефону", text="Номер Телефону")
 contact_list.grid(row=5, column=0, columnspan=2, pady=10)
 contact_list.column("ID", width=30)
 contact_list.column("Ім'я", width=100)
-contact_list.column("Email", width=150)
+contact_list.column("Номер Телефону", width=150)
 
 
 root.mainloop()
